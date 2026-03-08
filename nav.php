6230ccd9ab887f981f3231655a967e921382fc07
zjbradbury <88970824+zjbradbury@users.noreply.github.com>
2026-03-08 20:26:11 +1030

    2

---
+++ nav.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+    <ul>
+        <li><a href="/index.php">Home</a></li>
+
+ 
+                <li><a href="#">Placeholder</a></li>
+               <li><a href="#">Placeholder</a></li>
+               <li><a href="#">Placeholder</a></li>
+               <li><a href="#">Placeholder</a></li>
+
+<!--        <li><a href="login-page.html">Login</a></li>
+        <li><a href="../UpdateDisplay.php">Admin</a></li>-->
+        <li style="float:right"><a href="/login.php">Login</a></li>
+ <!--        <li style="float:right"><a href="/demo.php">Demo</a></li> -->
+    </ul>
+
+</body>
+
+</html>
